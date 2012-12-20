22600f0ae6bdb20b377eaac20ed225b5881c0025
Triston J. Taylor <pc.wiz.tt@gmail.com>
2012-12-19 22:38:29 -0600

    Added Embedd Licensing details
    Modified: hexstream.c
    Modified: streamhex.c

--- streamhex.c
+++ streamhex.c
@@ -1,3 +1,24 @@
+/*
+ * streamhex.c
+ * This file is part of base-16-streamer
+ *
+ * Copyright (C) 2012 - Triston J. Taylor
+ *
+ * base-16-streamer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * base-16-streamer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with base-16-streamer. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
